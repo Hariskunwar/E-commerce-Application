b15085c3829131d80cfdab4335d68654155f2151
Harish Kunwar <kunwarharish11@gmail.com>
2024-09-04 09:12:38 +0545

    add product filtering, sorting, pagination, and field limiting

--- controllers/productController.js
+++ controllers/productController.js
@@ -42,7 +42,50 @@ exports.createProduct=asyncErrorHandler(async (req,res,next)=>{
 
     //get list of product
     exports.getProducts=asyncErrorHandler(async (req,res,next)=>{
-      const products=await Product.find();
+
+      //excluding some query string property
+      const excludeFields=['sort','page','limit','fields'];
+      let queryObj={...req.query};
+      excludeFields.forEach((el)=>{
+        delete queryObj[el];
+      })
+
+      //filtering
+      let queryStr=JSON.stringify(queryObj);
+      queryStr=queryStr.replace(/\b(gte|gt|lte|lt)\b/g,(match)=>`$${match}`);
+      queryObj=JSON.parse(queryStr);
+
+      let query=Product.find(queryObj);
+
+      //sorting
+      if(req.query.sort){
+        let sortBy=req.query.sort.split(",").join(' ');
+        query=query.sortBy(sortBy);
+      }else{
+        query=query.sort('-createdAt');
+      }
+
+      //limiting fields
+      if(req.query.fields){
+        let fields=req.query.fields.split(",").join(' ');
+        query=query.select(fields);
+      }else{
+        query=query.select('-__v');
+      }
+
+      //pagination
+      const page=req.query.page*1||1;
+      const limit=req.query.limit*1||10;
+      const skip=(page-1)*limit;
+      query=query.skip(skip).limit(limit);
+      if(req.query.page){
+        const productCount=await Product.countDocuments();
+        if(skip>=productCount){
+          return next(new CustomError('This page is not found',404));
+        }
+      }
+
+      const products=await query;
       res.status(200).json({
         data:products
       });
