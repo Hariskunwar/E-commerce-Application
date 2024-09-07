c9d1c7b215c23fc6a8f64656d0481d727dd333fa
Harish Kunwar <kunwarharish11@gmail.com>
2024-09-07 13:54:44 +0545

    Add security measures

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express=require("express");
 const errorHandler=require("./middlewares/errorHandler");
 const CustomError=require("./utils/CustomError");
+const rateLimit=require("express-rate-limit");
+const helmet=require("helmet");
+
+const limiter=rateLimit({
+    max:100,
+    windowMs:15*60*1000,
+    message:"Too many request, Please try after one hour"
+});
 
 const app=express();
 
-app.use(express.json());
+app.use(helmet());
+app.use(express.json({limit:"20kb"}));
 app.use(express.urlencoded({extended:false}));
+
+//apply rate limiter
+app.use("/api",limiter);
 
 app.use("/api/v1/users",require("./routes/userRoute"));
 app.use("/api/v1/products",require("./routes/productRoute"));
